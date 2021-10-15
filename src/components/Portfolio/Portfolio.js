87c202903fe5cb7f4835794c5daf6366c2aeacfd
V.Mikhail <V.Mikhail.V@yandex.ru>
2021-10-15 22:06:29 +0300

    15102021

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,19 +6,19 @@ export default function Portfolio() {
       <h2 className='portfolio__title'>Портфолио</h2>
       <ul className='portfolio__links'>
         <li className='portfolio__list-element'>
-          <a href='https://vmikhailw.github.io/how-to-learn/' className='portfolio__link'
+          <a href='https://vmikhailw.github.io/how-to-learn/index.html' className='portfolio__link'
             target='_blank' rel='noreferrer'>Статичный сайт
             <img src={linkCursor} alt='Указатель ссылки' className='portfolio__link-cursor' />
           </a>
         </li>
         <li className='portfolio__list-element'>
-          <a href='https://eugenegordievsky.github.io/russian-travel' className='portfolio__link'
+          <a href='https://vmikhailw.github.io/russian-travel/index.html' className='portfolio__link'
             target='_blank' rel='noreferrer'>Адаптивный сайт
             <img src={linkCursor} alt='Указатель ссылки' className='portfolio__link-cursor' />
           </a>
         </li>
         <li className='portfolio__list-element'>
-          <a href='https://github.com/EugeneGordievsky/react-mesto-api-full.git' className='portfolio__link'
+          <a href='http://express.nomoredomains.club' className='portfolio__link'
             target='_blank' rel='noreferrer'>Одностраничное приложение
             <img src={linkCursor} alt='Указатель ссылки' className='portfolio__link-cursor' />
           </a>
