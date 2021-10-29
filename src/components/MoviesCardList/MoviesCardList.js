79cf7c8a6b3253b9565ce5d10717762d41253d49
V.Mikhail <V.Mikhail.V@yandex.ru>
2021-10-29 16:30:14 +0300

    29102021

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -21,7 +21,7 @@ export default function MoviesCardList(props) {
     <section className='movies-card-list'>
       { props.moviesList.length === 0 ?
         <p className='movies-card-list__not-found'>
-         Ничего не найдено
+          {props.resultBlockText}
         </p> :
         <div className='movies-card-list__container' >
           {
